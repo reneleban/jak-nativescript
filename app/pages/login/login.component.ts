af0fac98f4ffe9656b7a677ff028a47fce25b0f9
Jens Sproede <jens.sproede@gmail.com>
2016-12-12 12:02:42 +0100

    initial commit: project setup

---
+++ app/pages/login/login.component.ts
@@ -0,0 +1,68 @@
+import { Component, ElementRef, OnInit, ViewChild } from "@angular/core";
+import { User } from "../../shared/user/user";
+import { UserService } from "../../shared/user/user.service";
+import { Router } from "@angular/router";
+import { Page } from "ui/page";
+import { Color } from "color";
+import { View } from "ui/core/view";
+
+@Component({
+  selector: "my-app",
+  providers: [UserService],
+  templateUrl: "pages/login/login.html",
+  styleUrls: ["pages/login/login-common.css", "pages/login/login.css"]
+})
+
+export class LoginComponent implements OnInit {
+  user: User;
+  isLoggingIn = true;
+  @ViewChild("container") container: ElementRef;
+
+  constructor(private router: Router, private userService: UserService, private page: Page) {
+    this.user = new User();
+    this.user.email = "testtest@test.test";
+    this.user.password = "test123";
+  }
+
+  ngOnInit() {
+    this.page.actionBarHidden = true;
+    this.page.backgroundImage = "res://bg_login";
+  }
+
+  submit() {
+    if (this.isLoggingIn) {
+      this.login();
+    } else {
+      this.signUp();
+    }
+  }
+
+  login() {
+    this.userService.login(this.user)
+      .subscribe(
+        () => this.router.navigate(["/list"]),
+        (error) => alert("Account does not exist")
+      );
+  }
+
+  signUp() {
+    this.userService.register(this.user)
+      .subscribe(
+        () => {
+          alert("Your account was successfully created.");
+          this.toggleDisplay();
+        },
+        () => alert("Unfortunately we were unable to create your account.")
+      );
+  }
+
+  toggleDisplay() {
+    this.isLoggingIn = !this.isLoggingIn;
+
+    let container = <View>this.container.nativeElement;
+    container.animate({
+      backgroundColor: this.isLoggingIn ? new Color("white") : new Color("lightgray"),
+      duration: 200
+    });
+  }
+}
---
+++ app/shared/config.ts
@@ -0,0 +1,4 @@
+export class Config {
+  static apiUrl = "https://jak.codecamps.de/jak-login/";
+  static token = "";
+}
