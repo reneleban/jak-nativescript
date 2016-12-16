a1a0c6a2fb8639ebd874b7aa383480df0c98bdb4
René Leban <leban.rene@gmail.com>
2016-12-16 08:39:23 +0100

    remove predefined user / password string

--- app/pages/login/login.component.ts
+++ app/pages/login/login.component.ts
@@ -20,8 +20,8 @@ export class LoginComponent implements OnInit {
 
   constructor(private router: Router, private userService: UserService, private page: Page) {
     this.user = new User();
-    this.user.email = "testtest@test.test";
-    this.user.password = "test123";
+    //this.user.email = "testtest@test.test";
+    //this.user.password = "test123";
   }
 
   ngOnInit() {
