6abf4e224fdc0c1efd398d19c3c42feefcc35a40
Jens Sproede <jsproede@oev.de>
2017-01-23 13:23:15 +0100

    ifAndroid and ifIos directives, sidedrawer is now working on android and ios

--- app/app.module.ts
+++ app/app.module.ts
@@ -10,17 +10,22 @@ import { routes, navigatableComponents } from "./app.routing";
 import drawerModule = require("nativescript-telerik-ui/sidedrawer");
 import { SIDEDRAWER_DIRECTIVES } from "nativescript-telerik-ui/sidedrawer/angular";
 
+import { IfAndroidDirective, IfIosDirective } from "./shared/ifplatformdirectives";
+
 @NgModule({
   imports: [
     NativeScriptModule,
     NativeScriptHttpModule,
     NativeScriptFormsModule,
     NativeScriptRouterModule,
-    NativeScriptRouterModule.forRoot(routes)
+    NativeScriptRouterModule.forRoot(routes),
+
   ],
   declarations: [
     AppComponent,
     SIDEDRAWER_DIRECTIVES,
+    IfAndroidDirective,
+    IfIosDirective,
     ...navigatableComponents
   ],
   bootstrap: [AppComponent]
